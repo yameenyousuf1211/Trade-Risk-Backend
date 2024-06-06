db0900c05038a87af7e339395fba8180d2af606b
ali usaid <aliusaid55@gmail.com>
2024-06-06 05:44:43 -0700

    Attachment Work

--- src/controllers/auth/auth.controller.ts
+++ src/controllers/auth/auth.controller.ts
@@ -1,44 +1,45 @@
 import { Request, Response, NextFunction } from "express";
 import { asyncHandler, generatePassword, generateResponse, parseBody, sendEmail } from "../../utils/helpers";
-import {  createBanks, createUser, findUser } from "../../models";
+import { createBanks, createUser, findUser } from "../../models";
 import { ROLES, STATUS_CODES } from "../../utils/constants";
 import { IBank } from "../../interface";
 import uploadOnCloudinary from "../../utils/cloundinary";
 
 interface FileArray {
     image: Express.Multer.File[];
-  }
+}
 
 export const register = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-// register user
+    // register user
     const body = parseBody(req.body);
 
-        const isUserExist = await findUser({ email: body?.email });
+    const isUserExist = await findUser({ email: body?.email });
 
-        if (isUserExist) return next({
-            statusCode: STATUS_CODES.BAD_REQUEST,
-            message: 'User already exist'
-        });
+    if (isUserExist) return next({
+        statusCode: STATUS_CODES.BAD_REQUEST,
+        message: 'User already exist'
+    });
 
-     
-        const banks = await createBanks(body?.currentBanks);
-        req.body.password = generatePassword(); // password will be generate and send to user via email later he can change
 
-        
-        // if(!(req.files as unknown as FileArray).image){
-        // return next({
-        //     statusCode: STATUS_CODES.BAD_REQUEST,
-        //     message: 'PDF is required'
-        // });
-        // }
-        // const filePath = (req.files as unknown as FileArray).image[0]?.path ?? '';
-        
-        // req.body.authorizationPocLetter = (await uploadOnCloudinary(filePath))?.secure_url;
-        
-        const user = await createUser({ ...body, currentBanks: banks.map((bank: IBank) => bank._id) });
-        // const email = await sendEmail({ to: 'aliusaid55@gmail.com', subject: 'Welcome to our platform', html: `Your password is ${req.body.password}` });
-        // console.log(email);
-        
+    const banks = await createBanks(body?.currentBanks);
+    req.body.password = generatePassword(); // password will be generate and send to user via email later he can change
+
+    if (req.body.role === ROLES.CORPORATE) {
+        if (!(req.files as unknown as FileArray).image) {
+            return next({
+                statusCode: STATUS_CODES.BAD_REQUEST,
+                message: 'PDF is required'
+            });
+        }
+        const filePath = (req.files as unknown as FileArray).image[0]?.path ?? '';
+
+        req.body.authorizationPocLetter = (await uploadOnCloudinary(filePath))?.secure_url;
+    }
+
+    const user = await createUser({ ...body, currentBanks: banks.map((bank: IBank) => bank._id) });
+    // const email = await sendEmail({ to: 'aliusaid55@gmail.com', subject: 'Welcome to our platform', html: `Your password is ${req.body.password}` });
+    // console.log(email);
+
     generateResponse(user, "Register successful", res);
 });
 
@@ -53,9 +54,9 @@ export const login = asyncHandler(async (req: Request, res: Response, next: Next
     });
 
     // const isMatch = await user.isPasswordCorrect(body.password);            
-    
+
     //console.log(isMatch);
-    
+
     if (user.password != body.password) return next({
         statusCode: STATUS_CODES.UNAUTHORIZED,
         message: 'Invalid password'
@@ -74,7 +75,7 @@ export const login = asyncHandler(async (req: Request, res: Response, next: Next
 
 
 export const currentUser = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-   const user = await findUser({ _id: req.user._id });
+    const user = await findUser({ _id: req.user._id });
     generateResponse(user, 'User fetched successfully', res);
 });
 
