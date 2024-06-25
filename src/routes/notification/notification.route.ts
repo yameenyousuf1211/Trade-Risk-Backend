cfee4996c1ccffe05899119e71bfb67a62834441
ali usaid <aliusaid55@gmail.com>
2024-06-25 13:35:02 -0700

    saving notification in database 1 Create API to fetch Notification 2 update Notification 3 Delete Notification

--- src/routes/notification/notification.route.ts
+++ src/routes/notification/notification.route.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { notifications,subscribe } from "../../controllers";
+import { deleteNotifications, fetchNotifications, notifications,subscribe, updateNotifications } from "../../controllers";
 import authMiddleware from "../../middlewares/auth.middleware";
 import { ROLES } from "../../utils/constants";
 import { notificationValidation, subscriptionValidation } from "../../validation/notification/notification.validation";
@@ -13,7 +13,11 @@ export default class NotificationAPI {
     setupRoutes() {
         this.router.post('/send-notification',authMiddleware(Object.values(ROLES)),notificationValidation,notifications);
         this.router.post('/subscriptions',authMiddleware(Object.values(ROLES)),subscriptionValidation,subscribe);
+        this.router.get('/',authMiddleware(Object.values(ROLES)),fetchNotifications);
+        this.router.put('/',authMiddleware(Object.values(ROLES)),updateNotifications);
+        this.router.delete('/:id',authMiddleware(Object.values(ROLES)),deleteNotifications);
     }    
+
     getRouter() {
         return this.router;
     }
