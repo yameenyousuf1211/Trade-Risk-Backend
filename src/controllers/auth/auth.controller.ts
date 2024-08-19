aeb9f565649a8742eba00c45632f445fa2b50c08
Shahzad <shazee257@gmail.com>
2024-08-19 21:13:15 +0500

    login API updated with business populated

--- src/controllers/auth/auth.controller.ts
+++ src/controllers/auth/auth.controller.ts
@@ -36,7 +36,7 @@ export const register = asyncHandler(async (req: Request, res: Response, next: N
 export const login = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
     const body = parseBody(req.body);
 
-    let user = await findUser({ email: body?.email }).populate('business').select('+password');
+    let user = await findUser({ email: body?.email }).select('+password').populate('business');
     if (!user) return next({
         statusCode: STATUS_CODES.BAD_REQUEST,
         message: 'Invalid email or password'
@@ -54,8 +54,9 @@ export const login = asyncHandler(async (req: Request, res: Response, next: Next
     const accessToken = await user.generateAccessToken();
     req.session = { accessToken };
     if (body.fcmToken) {
-        user = await updateUser(user._id, { $addToSet: { fcmTokens: body.fcmToken } });
+        await updateUser(user._id, { $addToSet: { fcmTokens: body.fcmToken } });
     }
+
     // remove password
     user = user.toObject();
     delete user.password;
