6c88cee5fa020dc320386ee576d08c57cb7a8527
Shahzad <shazee257@gmail.com>
2024-10-07 18:13:11 +0500

    Add socket.io dependency and initialize socket.io server

---
+++ src/socket/index.ts
@@ -0,0 +1,45 @@
+import { Server, Socket } from "socket.io";
+import { Request } from "express";
+
+// // unread count event
+// const unreadCountEvent = (socket) => {
+//     socket.on("unread-count", async (userId) => {
+//         const notifications = await unreadNotifications(userId);
+//         socket.emit("unread-count", notifications[0]);
+//     });
+// }
+
+// // read all notifications event
+// const readAllEvent = (socket) => {
+//     socket.on("read-all", async (userId) => {
+//         await readNotifications({ receiver: userId });
+//     });
+// }
+
+export const initializeSocketIO = (io: Server) => {
+    return io.on("connection", async (socket: Socket) => {
+        try {
+            const { business } = socket.handshake.headers;
+            console.log('socket connected >>>>', business);
+
+            // join the room with user id
+            socket.join(business as string);
+
+            // Common events that needs to be mounted on the initialization
+            // unreadCountEvent(socket);
+            // readAllEvent(socket);
+
+            socket.on("disconnect", async () => {
+                console.log("user has disconnected..", business);
+            });
+
+        } catch (error: any) {
+            socket.emit("socket-error", error?.message || "Something went wrong while connecting to the socket.");
+        }
+    });
+};
+
+export const emitSocketEvent = (req: Request, roomId: string, event: string, payload: any): void => {
+    const io: Server = req.app.get("io") as Server;
+    io.in(roomId).emit(event, payload);
+};
