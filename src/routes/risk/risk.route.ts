1c2baaa53b37d19900c7187afe50769108fa42bf
Shahzad <shazee257@gmail.com>
2024-10-18 12:20:20 +0500

    Refactor risk controller and model to add editRisk functionality

--- src/routes/risk/risk.route.ts
+++ src/routes/risk/risk.route.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import authMiddleware from "../../middlewares/auth.middleware";
 import { ROLE_TYPES, ROLES } from "../../utils/constants";
 import { addRisk, fetchAllRisks } from "../../controllers";
+import { updateRisk } from "../../models";
 
 export default class RiskAPI {
     constructor(private readonly router: Router) {
@@ -13,7 +14,7 @@ export default class RiskAPI {
         this.router.get('/', authMiddleware(Object.values(ROLES), [ROLE_TYPES.BANK]), fetchAllRisks);
         this.router.post('/', authMiddleware(Object.values(ROLES), [ROLE_TYPES.BANK]), addRisk);
         // this.router.delete('/:id', authMiddleware(Object.values(ROLES), Object.values(ROLE_TYPES)), deleteRisks);
-        // this.router.put('/:id', upload("authorization").fields([{ name: 'authorization-letter', maxCount: 3 }]), authMiddleware(Object.values(ROLES), Object.values(ROLE_TYPES)), riskUpdate);
+        this.router.put('/:id', authMiddleware(Object.values(ROLES), [ROLE_TYPES.BANK]), updateRisk);
         // this.router.get('/:id', authMiddleware(Object.values(ROLES), Object.values(ROLE_TYPES)), findSingleRisk);
     }
 
